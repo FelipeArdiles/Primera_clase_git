f9a854824b7b52ab96283728ec9f3f2af2693eeb
felipeardiles <fe.ardiles@duocuc.cl>
2024-05-29 09:22:38 -0400

    cree mi diccionario del ejercicio

---
+++ ejemplo_git.py
@@ -0,0 +1,8 @@
+#Ejercicio diccionario de hoy: usuarios
+diccionario = {
+    "identificador":1,
+    "nombre": "marriott",
+    "direccion": "avenida bernardo o'higgins 1123",
+    "funcionando": True,
+    "valor_habitacion":100000
+}
